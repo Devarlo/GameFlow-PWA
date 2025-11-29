ccb4e63d780c30130aa936022b3fa693911b411c
Devarlo <devarlorazan05@gmail.com>
2025-11-29 14:35:57 +0700

    bazinga

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
-import SplashScreen from "../components/splash/SplashScreen";
+import SplashScreen from "../components/Splash/SplashScreen";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import DashboardPage from "../pages/DashboardPage";
